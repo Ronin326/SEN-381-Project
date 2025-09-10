19ca9659eec3796e9353f555349b25619c3c9ec8
Ronin326 <ruanleroux37@gmail.com>
2025-09-10 15:05:17 +0200

    Email validation on website is done

---
+++ CampusLearn Web App/wwwroot/scripts/LoginAndRegisterValidation.js
@@ -0,0 +1,9 @@
+﻿function ValidForm(){
+    const email = document.getElementById("email").value.trim();
+    const requiredDomain = "@belgiumcampus.ac.za";
+
+    if (!email.endsWith(requiredDomain)) {
+        event.preventDefault();
+        window.alert("Please use your Belgium Campus email address (must end with " + requiredDomain + ").");
+    }
+};
