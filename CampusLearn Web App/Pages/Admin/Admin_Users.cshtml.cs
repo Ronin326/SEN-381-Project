ecb9dd68ead56bec88f3e7227b2955b721330963
Ronin326 <ruanleroux37@gmail.com>
2025-10-20 09:56:17 +0200

    Admin Front end Is complete

---
+++ CampusLearn Web App/Pages/Admin/Admin_Users.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CampusLearn_Web_App.Pages.Admin
+{
+    public class Admin_UsersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
